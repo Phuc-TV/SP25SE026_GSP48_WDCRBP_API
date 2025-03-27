e61fb9e2a9a5cde88b60de8b7063df423f079eb0
Phuc-TV <tranvietphuc123654@gmail.com>
2025-03-27 21:46:18 +0700

    Add ServiceOrder

--- src/main/java/SP25SE026_GSP48_WDCRBP_api/service/ServiceOrderService.java
+++ src/main/java/SP25SE026_GSP48_WDCRBP_api/service/ServiceOrderService.java
@@ -1,4 +1,13 @@
 package SP25SE026_GSP48_WDCRBP_api.service;
 
+import SP25SE026_GSP48_WDCRBP_api.model.entity.ServiceOrder;
+
+import java.util.List;
+
 public interface ServiceOrderService {
+    List<ServiceOrder> listServiceOrderByUserIdOrWwId(Long id, String role);
+
+    ServiceOrder addServiceOrderCustomize(Long availableServiceId, Long userId, Long designIdeaVariantId);
+
+    ServiceOrder acceptServiceOrder(Long serviceOrderId);
 }
