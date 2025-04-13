dcaa8ec6e00fe4702a213fa6b3f22a2a1badbbb1
votrongluan <trongluan115@gmail.com>
2025-04-13 17:35:28 +0700

    update: remove order detail, hasDesign field and fix name spelling of guaranteeId

--- src/main/java/SP25SE026_GSP48_WDCRBP_api/repository/ProductOrderDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package SP25SE026_GSP48_WDCRBP_api.repository;
-
-import SP25SE026_GSP48_WDCRBP_api.model.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductOrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-}
