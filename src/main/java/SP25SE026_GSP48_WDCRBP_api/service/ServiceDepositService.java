6b3f0f44a7a86ef9012f0c62d9227f0ef7f16c65
namzuize <namndpse171442@fpt.edu.vn>
2025-04-23 22:40:05 +0700

    add 3 api

--- src/main/java/SP25SE026_GSP48_WDCRBP_api/service/ServiceDepositService.java
+++ src/main/java/SP25SE026_GSP48_WDCRBP_api/service/ServiceDepositService.java
@@ -1,11 +1,12 @@
 package SP25SE026_GSP48_WDCRBP_api.service;
 
 import SP25SE026_GSP48_WDCRBP_api.model.responseModel.ServiceWithDepositsRes;
+import SP25SE026_GSP48_WDCRBP_api.model.requestModel.UpdateServiceDepositPercentRequest;
 import SP25SE026_GSP48_WDCRBP_api.model.responseModel.UpdateServiceDepositPercentRes;
 
 import java.util.List;
 
 public interface ServiceDepositService {
-    List<UpdateServiceDepositPercentRes> updatePercentAllServiceDepositByServiceId(Long serviceId, Short newPercent);
+    List<UpdateServiceDepositPercentRes> updateDepositPercents(UpdateServiceDepositPercentRequest request);
     List<ServiceWithDepositsRes> getAllServiceWithDeposits();
 }
